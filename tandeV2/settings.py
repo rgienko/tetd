b8623a7fe633e22560d56d0ee9ebae75df837f3d
Randall.Gienko <rgienko@gmail.com>
2023-09-25 15:57:14 -0500

    Static file update

--- tandeV2/settings.py
+++ tandeV2/settings.py
@@ -146,7 +146,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
